6811e86dc220c3ead53a66b4df68b8396dadab00
vector516 <vector_516@163.com>
2018-08-03 08:17:50 +0800

    aaa

---
+++ src/com.itheima.mail/Test.java
@@ -0,0 +1,11 @@
+package com.itheima.mail;
+
+import javax.mail.MessagingException;
+
+public class Test {
+    public static void main(String[] args) throws MessagingException {
+    MailUtils.sendMail("wuyunlong_516@163.com","http://192.168.20.74:8080","问候");
+    System.out.println("搞定........");
+
+    }
+}
